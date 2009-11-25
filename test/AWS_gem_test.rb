ad06c3e0b7e994708c654ae48e4051253e8aecfd
buzzware <regos@buzzware.com.au>
2009-11-25 21:11:10 +0800

    cleaned up AWS S3 gem tests & methods

--- test/AWS_gem_test.rb
+++ test/AWS_gem_test.rb
@@ -35,41 +35,10 @@ class AWSGemTest < Test::Unit::TestCase
 		AWS::S3::Bucket.find(aName)
 	end
 
-	# yore 
-	# create bucket name
-	# ensure_clean_bucket
-
-	# test with methods for creating suitable acl
-
-	# see http://amazon.rubyforge.org/
-	#
-	# policy = S3Object.acl('kiss.jpg', 'marcel')
-  # pp policy.grants
-  # [#<AWS::S3::ACL::Grant FULL_CONTROL to noradio>,
-  #  #<AWS::S3::ACL::Grant READ to AllUsers Group>]
-
-	#policy = S3Object.acl('kiss.jpg', 'marcel')
-	#grant = ACL::Grant.new
-	#grant.permission = 'READ_ACP'
-	#grantee = ACL::Grantee.new
-	#grant.grantee = grantee
-	#policy.grants << grant
-	##pp policy.grants
-	#S3Object.acl('kiss.jpg', 'marcel', policy)
-	## after : pp S3Object.acl('kiss.jpg', 'marcel').grants
-
-## Add a grant to the bucket's policy
-#  bucket.acl.grants << some_grant
-#
-#  # Write the changes to the policy
-#  bucket.acl(bucket.acl)
-
 	# eg. policy = policy_add(policy,{'id' => 'dssdfsdf'},%w(READ WRITE))
 	def policy_add(aPolicy,aGranteeAttrs,aPermissions)
 		aPolicy ||= AWS::S3::ACL::Policy.new
 		grantee = AWS::S3::ACL::Grantee.new(aGranteeAttrs)
-		#grantee.email_address = 'familyincnet@gmail.com'
-		#grantee.email_address = aUserEmail
 		grantee.display_name ||= 'display_name'
 		aPermissions.each do |p|
 			grant = AWS::S3::ACL::Grant.new
@@ -79,20 +48,6 @@ class AWSGemTest < Test::Unit::TestCase
 		end
 		aPolicy
 	end
-
-		#policy = AWS::S3::Bucket.acl(aBucketName)
-		#grantee = AWS::S3::ACL::Grantee.new
-		##grantee.email_address = 'familyincnet@gmail.com'
-		#grantee.id = aUserId
-		#grantee.display_name = 'test_user'
-		#aPermissions.each do |p|
-		#	grant = AWS::S3::ACL::Grant.new
-		#	grant.permission = p
-		#	grant.grantee = grantee
-		#	policy.grants << grant
-		#end
-		#AWS::S3::Bucket.acl(aBucketName, policy)
-
 
 	def grant_bucket_permissions(aBucketName,aPermissions,aGranteeAttrs,aMerge = false)
 		policy = (aMerge ? AWS::S3::Bucket.acl(aBucketName) : nil)
@@ -110,9 +65,32 @@ class AWSGemTest < Test::Unit::TestCase
 		policy
 	end
 
+	# ensures the destination bucket exists with the right permissions for upload_backup
+	def ensure_backup_bucket(aBucketName,aOtherUserAttrs=nil)
+		AWS::S3::Bucket.create(aBucketName) unless bucket_exists?(aBucketName)
+		grant_bucket_permissions(aBucketName,%w(WRITE READ_ACP),aOtherUserAttrs,true) if aOtherUserAttrs
+	end
+
+	# Summary: Uploads the given file to the bucket, then gives up permissions to the bucket owner
+	# Details :
+	#	* intended to allow files to be uploaded to S3, but not allowing the files to be interfered with should 
+	#   the web server get hacked.
+	# 	In truth, S3 permissions aren't adequate and the best we can do is that the file can't be read,
+	# 	but can be written over. The user also can't get a listing of the bucket
+	# * S3 won't allow objects (or buckets) to change owner, but we do everything else ie give FULL_CONTROL,
+	# 	and remove it from self, to hand control to the bucket owner
+	# * This requires the bucket to give WRITE & READ_ACP permissions to this user
+	def upload_backup(aFileName,aBucketName,aObjectName = nil)
+		aObjectName ||= File.basename(aFileName)
+		AWS::S3::S3Object.store(aObjectName, MiscUtils.string_from_file(aFileName), aBucketName)
+		bucket_owner = AWS::S3::Bucket.acl(aBucketName).owner
+		policy = policy_add(nil,{'id' => bucket_owner.id, 'display_name' => bucket_owner.display_name},'FULL_CONTROL')
+		policy.owner = AWS::S3::Owner.current
+		# replace policy with full control to bucket owner, none to test_user
+		AWS::S3::S3Object.acl(aObjectName,aBucketName,policy)
+	end
+
 	should "login" do
-		#buckets = AWS::S3::Service.buckets
-		#assert !buckets.empty?
 		test_bucket = ensure_clean_bucket(CREDS[:s3_yore_test_bucket])
 		puts test_bucket.inspect
 		assert !test_bucket.nil?
@@ -127,9 +105,10 @@ class AWSGemTest < Test::Unit::TestCase
 		assert_equal test_content, after_content
 	end
 	
+
 	should "create bucket writable (and not readable) by another user" do
 			test_user_bucket = ensure_clean_bucket(CREDS[:s3_test_user_bucket])
-			grant_bucket_permissions(CREDS[:s3_test_user_bucket],%w(WRITE),{'email_address' => CREDS[:s3_test_user_email]},true)
+			ensure_backup_bucket(CREDS[:s3_test_user_bucket],{'email_address' => CREDS[:s3_test_user_email]})
 
 			test_filename = 'up_down_compare'
 			test_content = 'xyz'
@@ -138,20 +117,9 @@ class AWSGemTest < Test::Unit::TestCase
 				:access_key_id     => CREDS[:s3_test_user_id],
 				:secret_access_key => CREDS[:s3_test_user_key]
 			)
-			#test_user_bucket = AWS::S3::Bucket.find(CREDS[:s3_test_user_bucket])
+			temp_file = MiscUtils.make_temp_file(nil, nil, test_content)
+			upload_backup(temp_file,CREDS[:s3_test_user_bucket],test_filename)
 			
-			AWS::S3::S3Object.store(test_filename, test_content, CREDS[:s3_test_user_bucket])
-			#bucket_owner = AWS::S3::Bucket.acl(CREDS[:s3_test_user_bucket]).owner
-			#require 'ruby-debug'; debugger
-			#policy = policy_add(nil,{'email_address' => CREDS[:s3_test_user_email]},'FULL_CONTROL')
-			policy = policy_add(nil,{'email_address' => CREDS[:s3_access_email]},'FULL_CONTROL')
-			#policy.owner = bucket_owner
-			# can only give the current owner here. The docs say the owner of a bucket or object cannot be changed
-			policy.owner = AWS::S3::Owner.current
-			#policy.owner = AWS::S3::Owner.new('id' => bucket_owner.id, 'display_name' => bucket_owner.display_name)
-			# replace policy with full control to bucket owner, none to test_user
-			AWS::S3::S3Object.acl(test_filename,CREDS[:s3_test_user_bucket],policy)  #S3Object.acl('kiss.jpg', 'marcel')
-
 			# should fail reading
 			begin
 				after_content = AWS::S3::S3Object.value test_filename, CREDS[:s3_test_user_bucket]
@@ -179,6 +147,5 @@ class AWSGemTest < Test::Unit::TestCase
 			after_content = AWS::S3::S3Object.value test_filename, test_user_bucket.name
 			assert_equal test_content, after_content
 	end
-
 
 end
